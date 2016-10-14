b2cb765cc9aac69cc464064135a80764fd6a8874
jens-eriksson <jens.eriksson@it-e.se>
2016-10-14 15:56:05 +0200

    save all before commit :-)

--- app/app.js
+++ app/app.js
@@ -1,8 +1,7 @@
 (function () {
 
   var companyAnalysis = angular.module('companyAnalysis', ['ngComponentRouter']);
-  companyAnalysis.value('$routerRootComponent', 'appLayout');
-
+  companyAnalysis.value('$routerRootComponent', 'appLayout');  
 
 } ());
 
